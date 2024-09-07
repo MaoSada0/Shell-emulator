3e8876c882d2630ab9053f38cd992cb13d774d67
qqMao <666kakryatskii666@gmail.com>
2024-09-07 21:47:40 +0300

    add cat and --script

--- src/main/java/ru/qq/Shell.java
+++ src/main/java/ru/qq/Shell.java
@@ -1,10 +1,13 @@
 package ru.qq;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Shell {
-    private VirtualFileSystem vfs;
+    private static VirtualFileSystem vfs;
     private String username;
     private String hostname;
 
@@ -15,38 +18,79 @@ public class Shell {
         this.hostname = hostname;
     }
 
-    public void start() {
+    public void start() throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.print(username + "@" + hostname + ":~$ ");
             String input = scanner.nextLine();
-            String[] parts = input.trim().split("\\s+");
-            String command = parts[0];
-            String[] args = new String[parts.length - 1];
-            System.arraycopy(parts, 1, args, 0, parts.length - 1);
-
-            switch (command) {
-                case "exit":
-                    System.out.println("Exiting shell...");
-                    break;
-                case "ls":
-                    vfs.ls().forEach(System.out::println);
-                    break;
-                case "cd":
-                    if (args.length > 0) {
-                        vfs.cd(args[0]);
-                    } else {
-                        System.out.println("cd: missing argument");
-                    }
-                    break;
-                case "pwd":
-                    System.out.println(vfs.getCurrentDir());
-                    break;
-                default:
-                    System.out.println(command + ": command not found");
-                    break;
-            }
+            executeCommand(input);
         }
+    }
+
+    private static void executeScript(String scriptFilePath) {
+        File scriptFile = new File(scriptFilePath);
+        if (!scriptFile.exists()) {
+            System.err.println("Script file not found: " + scriptFilePath);
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(scriptFile))) {
+            String line = br.readLine();
+            while (line != null) {
+                executeCommand(line.trim());
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading script file: " + e.getMessage());
+        }
+    }
+
+    private static void executeCommand(String input) throws IOException {
+        if (input.isEmpty()) {
+            return;
+        }
+
+        String[] parts = input.trim().split("\\s+");
+        String command = parts[0];
+        String[] args = new String[parts.length - 1];
+        System.arraycopy(parts, 1, args, 0, parts.length - 1);
+
+        switch (command) {
+            case "exit":
+                System.out.println("Exiting shell...");
+                break;
+            case "ls":
+                vfs.ls().forEach(System.out::println);
+                break;
+            case "cd":
+                if (args.length > 0) {
+                    vfs.cd(args[0]);
+                } else {
+                    System.out.println("cd: missing argument");
+                }
+                break;
+            case "pwd":
+                System.out.println(vfs.getCurrentDir());
+                break;
+            case "cat":
+                if (args.length > 0) {
+                    vfs.readFile(args[0]);
+                } else {
+                    System.out.println("cat: missing argument");
+                }
+                break;
+            case "--script":
+                if (args.length > 0) {
+                    executeScript(args[0]);
+                } else {
+                    System.out.println("--script: missing argument");
+                }
+                break;
+            default:
+                System.out.println(command + ": command not found");
+                break;
+        }
+
+
     }
 }
--- src/main/java/ru/qq/VirtualFileSystem.java
+++ src/main/java/ru/qq/VirtualFileSystem.java
@@ -1,5 +1,6 @@
 package ru.qq;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.*;
 import java.util.zip.*;
@@ -69,6 +70,24 @@ public class VirtualFileSystem {
         }
         else if(tree.containsPath((currentDir + newDir).split("/"))) currentDir = currentDir + newDir + "/";
         else System.out.println(" No such file or directory");
+    }
+
+    public void readFile(String fileName) throws IOException {
+        String fullPath = currentDir + fileName;
+        ZipEntry entry = zipFile.getEntry(fullPath);
+
+        if (entry == null) {
+            System.out.println("cat: " + fileName + ": No such file");
+            return;
+        }
+
+        try (InputStream stream = zipFile.getInputStream(entry)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
     }
 
     public String getCurrentDir() {
